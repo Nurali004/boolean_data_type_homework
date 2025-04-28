dd6e239fb0658af244d44e80b554446f3f873f2d
M A C W I N <mavzurovnurali@gmail.com>
2025-04-29 00:36:52 +0500

    sss

--- bool01.py
+++ bool01.py
@@ -8,4 +8,9 @@ def main(a,b):
         bool
     """
     # Write your code here
-    return 
+    return a==b
+
+a=int(input("sonni kiriting:"))
+b=int(input("sonni kiriting:"))
+print(main(a,b))
+
